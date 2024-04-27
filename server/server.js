4fbcd5b5ab088b0a2f3b07bf6374f5700585544b
snipes <schneidsmc@gmail.com>
2024-04-27 13:56:32 -0500

    serverjs

--- server/server.js
+++ server/server.js
@@ -1,21 +1,68 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const db = require('./config/connection')
+const cors = require('cores');
+
+const { authMiddleware } = require('./utils/auth');
+
+const { ApolloServer, gql, ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  instrospection: true,
+  cache: 'bounded',
+  context: authMiddleware
+})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(_dirname, '../client/build')))
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'))
+})
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: '/graphql'})
+}
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () =>
+console.log(`API Server runngin on port ${PORT}`));
+console.log(`GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`)
+}
+);
+
+startApolloServer(typeDefs, resolvers);
+
+
+// const express = require('express');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
