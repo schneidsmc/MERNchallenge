5ff1b1d046219e3132394dfbb2a24caae2c7e0bc
snipes <schneidsmc@gmail.com>
2024-04-27 15:02:14 -0500

    utils mutations

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,9 +1,11 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo-client';
+import { LOGIN_USER } from '../utils/'
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
