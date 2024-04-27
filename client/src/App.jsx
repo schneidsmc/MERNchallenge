f4f141614245e446a3e0500334a18143dc6cb050
snipes <schneidsmc@gmail.com>
2024-04-27 14:39:42 -0500

    appjsx connect apollo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,36 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context'
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}`: '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <>
       <Navbar />
       <Outlet />
     </>
+    </ApolloProvider>
   );
 }
 
